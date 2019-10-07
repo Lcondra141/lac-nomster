08fa84da030d1982319e892281473aa2f7012cc6
Lcondra141 <53981877+Lcondra141@users.noreply.github.com>
2019-10-07 17:19:35 -0500

    added comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  geocoded_by :address
+  has_many :comments
+
+   geocoded_by :address
   after_validation :geocode
   
   validates :name, presence: true
